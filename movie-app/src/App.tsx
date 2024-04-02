4bb6d81f1e94a3aba0a6fd05ee1e6755761a4bf8
Luis F. Segura <122110623+luisfco0215@users.noreply.github.com>
2024-04-02 15:56:16 -0400

    Built a responsive layout.

---
+++ movie-app/src/App.tsx
@@ -0,0 +1,32 @@
+
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+
+
+function App() {
+
+  return (
+    <>
+      <Grid
+        templateAreas={
+          {
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+          }
+        }>
+        <GridItem bg='coral' area={'nav'}>
+          Nav
+        </GridItem>
+        <Show above='lg'>
+          <GridItem area={'aside'} bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg='dodgerblue' area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  )
+}
+
+export default App
