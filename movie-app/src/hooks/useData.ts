0047d7326735d409a4ceff7c2add320eb195a368
Luis F. Segura <122110623+luisfco0215@users.noreply.github.com>
2024-04-08 10:45:08 -0400

    change any type to complete deployment.

--- movie-app/src/hooks/useData.ts
+++ movie-app/src/hooks/useData.ts
@@ -10,7 +10,7 @@ interface FetchReponse<T> {
 const useData = <T>(
   endpoint: string,
   requestConfig?: AxiosRequestConfig,
-  deps?: any[]
+  deps?: unknown[]
 ) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
