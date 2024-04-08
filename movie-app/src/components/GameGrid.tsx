b33de5ba25bebd754faebec51878d0158d60dfe3
Luis Segura <luisfco0215@gmail.com>
2024-04-08 02:06:32 -0400

    Lil game grid change.

--- movie-app/src/components/GameGrid.tsx
+++ movie-app/src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import UseGames from "../hooks/UseGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -12,20 +12,19 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = UseGames(gameQuery);
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <p>{error}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={3}
-      >
-        {isLoading && skeleton.map((card) => <GameCardSkeleton key={card} />)}
-        {data.map((game) => (
-          <GameCard key={game.id} games={game} />
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding="10px"
+      spacing={3}
+    >
+      {isLoading && skeleton.map((card) => <GameCardSkeleton key={card} />)}
+      {data.map((game) => (
+        <GameCard key={game.id} games={game} />
+      ))}
+    </SimpleGrid>
   );
 };
 
