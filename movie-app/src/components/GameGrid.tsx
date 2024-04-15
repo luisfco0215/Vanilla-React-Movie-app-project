62519f248ffd628c0a62b3a3cd427ba5a3845fa4
Luis Segura <luisfco0215@gmail.com>
2024-04-14 20:01:21 -0400

    Fix some bugs.

--- movie-app/src/components/GameGrid.tsx
+++ movie-app/src/components/GameGrid.tsx
@@ -4,20 +4,21 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { GameQuery } from "../App";
 import React from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, isLoading, error, hasNextPage, fetchNextPage } = UseGames(gameQuery);
+  const { data, isLoading, error, hasNextPage, fetchNextPage } =
+    UseGames(gameQuery);
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   if (error) return <Text>{error.message}</Text>;
 
-  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
 
   return (
     <InfiniteScroll
@@ -26,9 +27,10 @@ const GameGrid = ({ gameQuery }: Props) => {
       hasMore={hasNextPage}
       loader={<Spinner textAlign="center" />}
       endMessage={
-        <p style={{ textAlign: 'center' }}>
+        <p style={{ textAlign: "center" }}>
           <b>Yay! You have seen it all</b>
-        </p>}
+        </p>
+      }
     >
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
@@ -37,11 +39,12 @@ const GameGrid = ({ gameQuery }: Props) => {
       >
         {isLoading && skeleton.map((card) => <GameCardSkeleton key={card} />)}
         {data?.pages.map((page) =>
-          page?.results.map((game, index) =>
+          page?.results.map((game, index) => (
             <React.Fragment key={index}>
               <GameCard key={game.id} games={game} />
             </React.Fragment>
-          ))}
+          ))
+        )}
       </SimpleGrid>
     </InfiniteScroll>
   );
