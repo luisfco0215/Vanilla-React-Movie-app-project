7197a2f4f9167c9632c3ae51328578d0fa032238
Luis F. Segura <122110623+luisfco0215@users.noreply.github.com>
2024-04-15 14:27:58 -0400

    resolved conflicts coming from Luis branch.

--- movie-app/src/components/GameGrid.tsx
+++ movie-app/src/components/GameGrid.tsx
@@ -3,24 +3,13 @@ import UseGames from "../hooks/UseGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import React from "react";
-<<<<<<< HEAD
 import InfiniteScroll from 'react-infinite-scroll-component';
 import useGameStore from "../store/gameStore";
 
-=======
-import InfiniteScroll from "react-infinite-scroll-component";
->>>>>>> 62519f248ffd628c0a62b3a3cd427ba5a3845fa4
 
-
-<<<<<<< HEAD
 const GameGrid = () => {
   const gameQuery = useGameStore((state) => state.gameQuery);
   const { data, isLoading, error, hasNextPage, fetchNextPage } = UseGames(gameQuery);
-=======
-const GameGrid = ({ gameQuery }: Props) => {
-  const { data, isLoading, error, hasNextPage, fetchNextPage } =
-    UseGames(gameQuery);
->>>>>>> 62519f248ffd628c0a62b3a3cd427ba5a3845fa4
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   if (error) return <Text>{error.message}</Text>;
